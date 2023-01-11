48110c4b35b327afc161cd22eb421ef50f192ec5
Uilian Fragallo <84922407+fragallou@users.noreply.github.com>
2023-01-11 08:36:02 -0300

    Edição e Melhorias
    Criação de modal para edição de dados, comentários e outras melhorias de código

--- temp.js
+++ temp.js
@@ -22,6 +22,9 @@ $(document).ready(function () {
     $("#data").val(moment().format('YYYY-MM-DD'));
 });
 
+/**
+ * LANÇAMENTOS
+ */
 var tempo = {
 
     /**
@@ -114,30 +117,87 @@ var tempo = {
         this.inserir(row);
     },
 
+    /**
+     * Armazenar todos os dados registrados
+     */
     armazenar: function () {
         let dados = $table.bootstrapTable('getData');
         localStorage.setItem('temp', JSON.stringify(dados));
     },
 
+    /**
+     * Eliminar todos os registros disponíveis
+     */
     limpar: function () {
         if (confirm("Esta operação é irreversível. Tem certeza que deseja limpar todos os registros?")) {
             localStorage.removeItem('temp');
             location.reload();
         }
+    },
+
+    /**
+     * Editar lançamento
+     * @param {object} row : linha com dados para edição
+     * @param {integer} index : índice da linha na tabela
+     */
+    editar: function (row, index) {
+        $("#editModal").modal('show');
+        $("#edicao_data").val(moment(row.data).format('YYYY-MM-DD'));
+        $("#edicao_projeto").val(row.projeto)
+        $("#edicao_inicio").val(moment(row.inicio).format('HH:mm'))
+        $("#edicao_fim").val(moment(row.fim).format("HH:mm"));
+        $("#edicao_atividade").val(row.atividade)
+    },
+
+    /**
+     * Salvar alterações da edição do registro
+     */
+    salvar: function () {
+        let obj = {
+            data: $("#edicao_data").val(),
+            projeto: $("#edicao_projeto").val(),
+            inicio: $("#edicao_inicio").val(),
+            fim: $("#edicao_fim").val(),
+            atividade: $("#edicao_atividade").val(),
+        }
+        this.atualizar(obj, index);
     }
 }
 
+/**
+ * FORMATAÇÕES
+ */
 var formatter = {
 
-
+    /**
+     * Formata Data
+     * @param {date} value : valor a ser formatado
+     * @param {object} row : linha completa
+     * @param {integer} index : índice da linha na tabela
+     * @returns date
+     */
     data: function (value, row, index) {
         return value != null ? moment(value).format('DD/MM/YYYY') : '-';
     },
 
+    /**
+     * Formata Hora
+     * @param {date} value : valor a ser formatado
+     * @param {object} row : linha completa
+     * @param {integer} index : índice da linha na tabela
+     * @returns time
+     */
     hora: function (value, row, index) {
         return value != null ? moment(value).format('HH:mm') : '-';
     },
 
+    /**
+     * Formata total de horas
+     * @param {date} value : valor a ser formatado
+     * @param {object} row : linha completa
+     * @param {integer} index : índice da linha na tabela
+     * @returns duration
+     */
     total: function (value, row) {
         return value != '00:00' ? value : "<span class='text-muted'>00:00</span>";
     },
@@ -151,19 +211,30 @@ var formatter = {
     acoes: function (value, row, index) {
         let options = "";
         if (row.fim == null) {
-            options += "<a href='#' class='me-2 text-danger' onclick='tempo.parar()'>Parar</a>";
+            options += "<a href='#' class='me-2 text-danger' onclick='tempo.parar()' title='Parar'><i class='fas fa-circle-stop me-2'></i></a>";
         } else {
-            options += "<a href='#' class='me-2 text-primary' onclick='tempo.continuar(" + JSON.stringify(row) + ")'>Continuar</a>";
+            options += "<a href='#' class='me-2 text-success' onclick='tempo.continuar(" + JSON.stringify(row) + ")' title='Continuar'><i class='fas fa-circle-play me-2'></i></a>";
+            options += "<a href='#' class='me-2 text-primary' onclick='tempo.editar(" + JSON.stringify(row) + "," + index + ")' title='Editar'><i class='fas fa-square-pen me-2'></i></a>";
         }
         return options;
     },
 
+    /**
+     * Formata número com zeros à esquerda
+     * @param {integer} num : valor a ser formatado
+     * @param {integer} size : número de zeros à esquerda
+     * @returns string
+     */
     padLeft: function (num, size) {
         var s = "000000000" + num;
         return s.substr(s.length - size);
     }
 }
 
+/**
+ * Botões de exportação de dados
+ * @returns functions
+ */
 function buttons() {
     return {
         btnExcel: {
